9c229bb6f1b54d8a54bf2451da642a84020b8489
SlavJuan <62333541+SlavJuan@users.noreply.github.com>
2021-03-16 09:43:54 +0100

    delete

--- Discord Bot/commands/help.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-
-module.exports = {
-    name: "-help",
-    aliases: ['h'],
-    async run(message, args, client) {
-        message.channel.send('**HELP**\n-h, --help: Provides Help information for Terminal commands\n-p, --ping: Provides Latency between bot and api')
-    }
-}
